2a7f2fd26776b3970541420730d413cce8de0447
Justice Payne <spikepayne@yahoo.com>
2020-08-30 01:48:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,4 +6,20 @@ def sort_array_desc(integers)
   integers.sort{|a,b| b<=>a}
 end
 
-def
+def sort_array_char_count(strings)
+  strings.sort{|a,b| a.length<=>b.length}
+end
+
+def swap_elements(array)
+  array.sort{|a,b| a[1]<=>b[2]}
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker($)
+  array.collect{|arr| array.delete(arry[2]).insert(2,"$")}
+end
+  
+  
